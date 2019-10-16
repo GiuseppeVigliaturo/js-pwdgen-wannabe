f8c7bace599212c48c8d1a1d032a7d2db90e42c9
giuviglia94 <vigliaturo94@gmail.com>
2019-10-16 16:40:33 +0200

    esercizio senza effetti

---
+++ assets/main.js
@@ -0,0 +1,18 @@
+
+// richiesta nome
+var nome = prompt('INSERICI IL TUO NOME');
+
+// richiesta cognome
+var cognome = prompt('INSERICI IL TUO COGNOME');
+
+// richiesta colore
+var colore = prompt('INSERICI IL TUO COLORE PREFERITO');
+// variabile saluto
+var saluto ="ciao "+nome;
+
+//variabile output finale
+var output="La tua password è "+nome+cognome+colore+"19";
+
+document.getElementById("ciao").innerHTML= saluto;
+
+document.getElementById("password").innerHTML= output;
