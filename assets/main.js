988d07e881ba36eb91a377c81d6e5fe05c274178
giuviglia94 <vigliaturo94@gmail.com>
2019-10-16 18:31:19 +0200

    effetto

--- assets/main.js
+++ assets/main.js
@@ -8,7 +8,7 @@ var cognome = prompt('INSERICI IL TUO COGNOME');
 // richiesta colore
 var colore = prompt('INSERICI IL TUO COLORE PREFERITO');
 // variabile saluto
-var saluto ="ciao "+nome;
+var saluto ="ciao "+nome+" "+cognome;
 
 //variabile output finale
 var output="La tua password è "+nome+cognome+colore+"19";
